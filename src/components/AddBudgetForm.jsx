06fd8d1f7eca2c3cf7e2e03dd2500e5a685435e8
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-21 16:55:27 -0500

    create add budget component

---
+++ src/components/AddBudgetForm.jsx
@@ -0,0 +1,51 @@
+import { Form } from "react-router-dom";
+
+export function AddBudgetForm() {
+  return (
+    <div className="form-wrapper">
+      <h2 className="h3">Create budget</h2>
+      <Form method="post" className="grid-sm">
+        <div className="grid-xs">
+          <label htmlFor="newBudget">Budget Name</label>
+          <input
+            type="text"
+            name="newBudget"
+            id="newBudget"
+            placeholder="e.g., Groceries"
+            required
+          />
+        </div>
+        <div className="grid-xs">
+          <label htmlFor="newBudgetAmount">Amount</label>
+          <input
+            type="number"
+            step="0.01"
+            name="newBudgetAmount"
+            id="newBudgetAmount"
+            placeholder="e.g., $350"
+            required
+            inputMode="decimal"
+          />
+        </div>
+        <button type="submit" className="btn btn--dark">
+          <span>Create budget</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+            width={25}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </button>
+      </Form>
+    </div>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,10 +5,12 @@ import { Intro } from "../components/Intro";
 import { fetchData } from "../helpers";
 
 import { toast } from "react-toastify";
+import { AddBudgetForm } from "../components/AddBudgetForm";
 
 export const dashboardLoader = () => {
   const userName = fetchData("userName");
-  return { userName };
+  const budgets = fetchData("budgets");
+  return { userName, budgets };
 };
 
 //action
@@ -34,7 +36,27 @@ export async function dashboardAction({ request }) {
 // }
 
 export function Dashboard() {
-  const { userName } = useLoaderData();
+  const { userName, budgets } = useLoaderData();
 
-  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back, <span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-sm">
+            {/* {budgets ? } */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 }
